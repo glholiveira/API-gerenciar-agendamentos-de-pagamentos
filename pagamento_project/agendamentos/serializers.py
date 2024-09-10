d8f6aa23a34659c8da4eb570ffd397e42832dce8
glholiveira <150027922+glholiveira@users.noreply.github.com>
2024-09-10 18:09:50 -0300

    correção retorno

--- pagamento_project/agendamentos/serializers.py
+++ pagamento_project/agendamentos/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Agendamento
 
-class AgendamentoSerializer(serializers.ModelSerializer):
+class AgendamentoSerializerGravacao(serializers.ModelSerializer):
     valor_pagamento = serializers.DecimalField(max_digits=10, decimal_places=2, coerce_to_string=False)
 
     class Meta:
@@ -11,3 +11,13 @@ class AgendamentoSerializer(serializers.ModelSerializer):
     def validate_valor_pagamento(self, value):
         # Converte o valor decimal para inteiro antes de salvar
         return int(value * 100)  # Multiplica por 100 para preservar até 2 casas decimais
+
+
+class AgendamentoSerializerLeitura(serializers.ModelSerializer):
+    valor_pagamento = serializers.IntegerField()
+
+    class Meta:
+        model = Agendamento
+        fields = '__all__'
+
+    
