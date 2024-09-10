78cd2194be753b06d658ea8fa1d405e16fea150b
glholiveira <150027922+glholiveira@users.noreply.github.com>
2024-09-10 15:24:54 -0300

    Criação API COM README.MD

---
+++ pagamento_project/agendamentos/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Agendamento
+
+class AgendamentoSerializer(serializers.ModelSerializer):
+    valor_pagamento = serializers.DecimalField(max_digits=10, decimal_places=2, coerce_to_string=False)
+
+    class Meta:
+        model = Agendamento
+        fields = '__all__'
+
+    def validate_valor_pagamento(self, value):
+        # Converte o valor decimal para inteiro antes de salvar
+        return int(value * 100)  # Multiplica por 100 para preservar até 2 casas decimais
